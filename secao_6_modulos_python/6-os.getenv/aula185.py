ad68921fc23d4ae4a2a706ba98222adf80604d07
Daniel B <danielbarretodeoliveira@gmail.com>
2023-04-03 11:24:35 -0300

    Nono dia de aula

---
+++ secao_6_modulos_python/6-os.getenv/aula185.py
@@ -0,0 +1,7 @@
+'''
+Aula 185
+Seção - 6 modulos python    (03/04/23)
+videos: 302, 303
+
+'Enviando E-mail SMTP'
+'''
---
+++ secao_6_modulos_python/7-zip_file/aula186.py
@@ -0,0 +1,50 @@
+# ZIP - Compactando / Descompactando arquivos com zipfile.ZipFile
+import os
+import shutil
+from pathlib import Path
+from zipfile import ZipFile
+
+# Caminhos
+CAMINHO_RAIZ = Path(__file__).parent
+
+CAMINHO_ZIP_DIR = CAMINHO_RAIZ / 'aula_186_diretorio_zip'
+CAMINHO_COMPACTADO = CAMINHO_RAIZ / 'aula186_compactado.zip'
+CAMINHO_DESCOMPACTADO = CAMINHO_RAIZ / 'aula186_descompactado'
+
+shutil.rmtree(CAMINHO_ZIP_DIR, ignore_errors=True)
+Path.unlink(CAMINHO_COMPACTADO, missing_ok=True)
+shutil.rmtree(str(CAMINHO_COMPACTADO).replace('.zip', ''), ignore_errors=True)
+shutil.rmtree(CAMINHO_DESCOMPACTADO, ignore_errors=True)
+
+# raise Exception()
+
+# Cria o diretório para a aula
+CAMINHO_ZIP_DIR.mkdir(exist_ok=True)
+
+
+def criar_arquivos(qtd: int, zip_dir: Path):
+    for i in range(qtd):
+        texto = 'arquivo_%s' % i
+        with open(zip_dir / f'{texto}.txt', 'w') as arquivo:
+            arquivo.write(texto+\
+                        "\nTESTE TESTE TESTE TESTE TESTE"*100)
+
+criar_arquivos(10, CAMINHO_ZIP_DIR)
+
+
+# Criando um zip e adicionando arquivos
+with ZipFile(CAMINHO_COMPACTADO, 'w') as zip:
+    for root, dirs, files in os.walk(CAMINHO_ZIP_DIR):
+        for _file in files:
+            zip.write(os.path.join(root, _file), _file)
+
+
+# Lendo arquivos de um zip
+with ZipFile(CAMINHO_COMPACTADO, 'r') as zip:
+    for _file in zip.namelist():
+        print(_file)
+
+
+# Extraindo arquivos de um zip
+with ZipFile(CAMINHO_COMPACTADO, 'r') as zip:
+    zip.extractall(CAMINHO_DESCOMPACTADO)
